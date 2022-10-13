03494bf0d901c51033f92acd5faee95e68a5aa89
Halaume <guillaume.hanquer@gmail.com>
2022-10-13 15:14:19 +0200

    Proto vector youpi

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -34,19 +34,68 @@ namespace ft
 //				typedef const iterator const_iterator;
 //				typedef reverse_iterator reverse_iterator;
 //				typedef const reverse_iterator const_reverse_iterator;
-				vector(void);
+				vector(void) {}
 				explicit vector(const Allocator & alloc);
 				explicit vector(std::size_t count, const T & value = T(), const Allocator & alloc = Allocator());
 				template<class InputIt>
 				vector(InputIt first, InputIt last, const Allocator & alloc = Allocator());
 				vector(const vector & other);
-				~vector(void);
+				~vector(void) {}
 				vector &	operator=(const vector & src);
 				void	assign(std::size_t count, const T & value);
 				template<class InputIt>
 				void	assign(InputIt first, InputIt last);
-
-
+				allocator_type	get_allocator() const
+				{
+					return  (this->Allocator);
+				}
+				reference at(std::size_t pos);
+				const_reference at(std::size_t pos) const;
+				reference operator[](size_type pos);
+				const_reference operator[](size_type pos) const;
+				reference	front(void)
+				{
+					return (*this->begin());
+				}
+				const_reference	front(void) const
+				{
+					return (*this->begin());
+				}
+				reference	back(void)
+				{
+					return (*this->end() - 1);
+				}
+				const_reference	back(void) const
+				{
+					return (*this->end - 1);
+				}
+				T*	data(void);
+				const T*	data(void) const;
+//				iterator	begin(void);
+//				const_iterator	begin(void) const;
+//				iterator	end(void);
+//				const_iterator	end(void) const;
+//				reverse_iterator	rbegin(void);
+//				const_reverse_iterator	rbegin(void) const;
+//				reverse_iterator	rend(void);
+//				const_reverse_iterator	rend(void) const;
+				bool	empty(void) const;
+				size_type	size(void) const;
+				size_type	max_size(void) const;
+				void	reserve(size_type new_cap);
+				size_type	capacity() const;
+				void	clear(void);
+//				iterator	insert(const_iterator pos, const T & value);
+//				iterator	insert(const_iterator pos, size_type count, const T & value);
+//				iterator	insert(const_iterator pos, size_type count, const T & value);
+//				template<class InputIt>
+//				iterator	insert(const_iterator pos, InputIt first, InputIt last);
+//				iterator	erase(iterator pos);
+//				iterator	erase(iterator first, iterator last);
+				void	push_back(const T & value);
+				void	pop_back(void);
+				void	resize(size_type count, T value = T());
+				void	swap(vector & other);
 
 		};
 }
