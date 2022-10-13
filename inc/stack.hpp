a38ae82f54e74f635ca341a0ecc6b1534331450d
Halaume <guillaume.hanquer@gmail.com>
2022-10-13 12:35:13 +0200

    Ui

--- inc/stack.hpp
+++ inc/stack.hpp
@@ -97,4 +97,5 @@ namespace ft
 				Container	c;
 		};
 }
+
 #endif
--- inc/vector.hpp
+++ inc/vector.hpp
@@ -22,6 +22,31 @@ namespace ft
 		class vector
 		{
 			public:
+				typedef T value_type;
+				typedef Allocator allocator_type;
+				typedef std::size_t size_type;
+//				typedef std::ptrdiff_t difference_type;
+				typedef T & reference;
+				typedef const T & const_reference;
+				typedef typename Allocator::pointer pointer;
+				typedef const typename Allocator::pointer const_pointer;
+//				typedef iterator iterator;
+//				typedef const iterator const_iterator;
+//				typedef reverse_iterator reverse_iterator;
+//				typedef const reverse_iterator const_reverse_iterator;
+				vector(void);
+				explicit vector(const Allocator & alloc);
+				explicit vector(std::size_t count, const T & value = T(), const Allocator & alloc = Allocator());
+				template<class InputIt>
+				vector(InputIt first, InputIt last, const Allocator & alloc = Allocator());
+				vector(const vector & other);
+				~vector(void);
+				vector &	operator=(const vector & src);
+				void	assign(std::size_t count, const T & value);
+				template<class InputIt>
+				void	assign(InputIt first, InputIt last);
+
+
 
 		};
 }
