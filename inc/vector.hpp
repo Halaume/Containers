1c0eddc625697681277417f01dd88a441b9c3deb
Halaume <guillaume.hanquer@gmail.com>
2022-10-14 10:43:29 +0200

    Aled

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -14,6 +14,7 @@
 #define VECTOR_HPP
 
 #include <memory>
+#include <stdexcept>
 #include "vector.hpp"
 
 namespace ft
@@ -35,13 +36,40 @@ namespace ft
 //				typedef reverse_iterator reverse_iterator;
 //				typedef const reverse_iterator const_reverse_iterator;
 				vector(void) {}
-				explicit vector(const Allocator & alloc);
-				explicit vector(std::size_t count, const T & value = T(), const Allocator & alloc = Allocator());
+				explicit vector(const Allocator & alloc)
+				{
+					this->Allocator = alloc;
+					this->_tab = new T*;
+					this->_size = 0;
+
+				}
+				explicit vector(std::size_t count, const T & value = T(), const Allocator & alloc = Allocator())
+				{
+					this->_size = count;
+					this->_tab = new T*;
+				}
 				template<class InputIt>
-				vector(InputIt first, InputIt last, const Allocator & alloc = Allocator());
-				vector(const vector & other);
+				vector(InputIt first, InputIt last, const Allocator & alloc = Allocator())
+				{
+					//this->_size = my_dist(first, last);
+					this->_tab = new T*;
+				}
+				vector(const vector & copy)
+				{
+					*this = copy;
+				}
 				~vector(void) {}
-				vector &	operator=(const vector & src);
+				vector &	operator=(const vector & src)
+				{
+					if (this == &src)
+						return (*this);
+					if (this->tab)
+						delete [] this->tab;
+					T* tmp = new T*;
+					//tmp = src._tab; //C'est de la bite en bois
+					this->_tab = tmp;
+					this->_size = src.size;
+				}
 				void	assign(std::size_t count, const T & value);
 				template<class InputIt>
 				void	assign(InputIt first, InputIt last);
@@ -49,10 +77,26 @@ namespace ft
 				{
 					return  (this->Allocator);
 				}
-				reference at(std::size_t pos);
-				const_reference at(std::size_t pos) const;
-				reference operator[](size_type pos);
-				const_reference operator[](size_type pos) const;
+				reference at(std::size_t pos)
+				{
+					if (pos < 0 || pos > this->_size)
+						throw std::out_of_range();
+					return (this->_tab[pos]);
+				}
+				const_reference at(std::size_t pos) const
+				{
+					if (pos < 0 || pos > this->_size)
+						throw std::out_of_range();
+					return (this->_tab[pos]);
+				}
+				reference operator[](size_type pos)
+				{
+					return (this->_tab[pos]);
+				}
+				const_reference operator[](size_type pos) const
+				{
+					return (this->_tab[pos]);
+				}
 				reference	front(void)
 				{
 					return (*this->begin());
@@ -67,7 +111,7 @@ namespace ft
 				}
 				const_reference	back(void) const
 				{
-					return (*this->end - 1);
+					return (*this->end() - 1);
 				}
 				T*	data(void);
 				const T*	data(void) const;
@@ -79,12 +123,26 @@ namespace ft
 //				const_reverse_iterator	rbegin(void) const;
 //				reverse_iterator	rend(void);
 //				const_reverse_iterator	rend(void) const;
-				bool	empty(void) const;
-				size_type	size(void) const;
-				size_type	max_size(void) const;
+				bool	empty(void) const
+				{
+					if (this->_size == 0)
+						return (true);
+					return (false);
+				}
+				size_type	size(void) const
+				{
+					return (this->_size);
+				}
+				size_type	max_size(void) const
+				{
+					return (Allocator::max_size());
+				}
 				void	reserve(size_type new_cap);
 				size_type	capacity() const;
-				void	clear(void);
+				void	clear(void)
+				{
+				//	iterator
+				}
 //				iterator	insert(const_iterator pos, const T & value);
 //				iterator	insert(const_iterator pos, size_type count, const T & value);
 //				iterator	insert(const_iterator pos, size_type count, const T & value);
@@ -92,11 +150,21 @@ namespace ft
 //				iterator	insert(const_iterator pos, InputIt first, InputIt last);
 //				iterator	erase(iterator pos);
 //				iterator	erase(iterator first, iterator last);
-				void	push_back(const T & value);
-				void	pop_back(void);
+				void	push_back(const T & value)
+				{
+					this->_tab[size] = value;
+					size++;
+				}
+				void	pop_back(void)
+				{
+					size--;
+					delete this->_tab[size];
+				}
 				void	resize(size_type count, T value = T());
 				void	swap(vector & other);
-
+			private:
+				T*			_tab;
+				size_type	_size;
 		};
 }
 
